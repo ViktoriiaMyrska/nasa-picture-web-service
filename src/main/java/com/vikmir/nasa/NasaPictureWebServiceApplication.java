4989d19636de8d1c1c6dfeba46af8ecf8210f875
victoria <victoria.mirskaya@mustpay.tech>
2022-07-10 21:45:17 +0100

    Initial commit

---
+++ src/main/java/com/vikmir/nasa/NasaPictureWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vikmir.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaPictureWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NasaPictureWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vikmir/nasa/NasaPictureWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikmir.nasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaPictureWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
